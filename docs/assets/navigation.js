7556c02571a50969a9583607245203e9664aea01
Erik Nielsen <erik@312development.com>
2024-01-31 01:43:04 -0500

    Pre-release: generating docs and formatting files

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslLyLyjJzM8rVtJRKkgsyVCyUsrMK0ktSktMTi3Wh8rpZZTk5ijpKGVn5qUoWRmZmtXqwLUHpZaUFuWFJeaUpmI1AkkenzHJ+QWVrnllCCPSSvOSwZbrQ6VQdZuZ1MYCAKKKpCfEAAAA"
